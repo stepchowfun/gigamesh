3c6e26a178a0d32cf6969ad04c21cfaacf77c348
Stephan Boyer <stephan@stephanboyer.com>
2020-06-16 19:05:19 -0700

    Add a logo

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import "./script/main.tsx";
-import attachFastClick from "fastclick";
-
-// FastClick eliminates the 300ms delay between a physical tap and the firing of a click event on
-// mobile browsers.
-attachFastClick(document.body);
