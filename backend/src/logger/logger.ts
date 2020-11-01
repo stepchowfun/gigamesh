7f22c8d05eb4905dca0ea10bfede0ebed1b94c62
Stephan Boyer <stephan@stephanboyer.com>
2020-11-01 12:40:42 -0800

    Scrub secrets from logs

--- backend/src/logger/logger.ts
+++ backend/src/logger/logger.ts
@@ -1,8 +1,39 @@
+import _ from 'lodash';
+import traverse from 'traverse';
 import { createLogger, format, transports } from 'winston';
+
+const secretFields = ['logInInvitationId', 'sessionId', 'signUpInvitationId'];
+
+const redactedValue = '[REDACTED]';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function scrubSecrets(payload: {}): any {
+  // To scrub secrets, we mutate the object with `traverse(...).forEach(...)`.
+  // To avoid mutating the input directly, we do a deep copy here first. We'd
+  // prefer to just use `traverse(...).map(...)` instead, but unfortunately
+  // we the `traverse` library has a bug that prevents it from traversing keys
+  // that are symbols, and `winston` uses symbolic keys for internal state.
+  // This is the bug: https://github.com/substack/js-traverse/issues/65
+  const payloadClone = _.cloneDeep(payload);
+
+  traverse(payloadClone).forEach(function scrubSecretsFromElement() {
+    secretFields.forEach((secretField) => {
+      if (this.key === secretField) {
+        this.update(redactedValue);
+      }
+    });
+  });
+
+  return payloadClone;
+}
 
 const logger = createLogger({
   level: 'info',
-  format: format.combine(format.timestamp(), format.json()),
+  format: format.combine(
+    format(scrubSecrets)(),
+    format.timestamp(),
+    format.json(),
+  ),
   transports: [new transports.Console()],
 });
 
