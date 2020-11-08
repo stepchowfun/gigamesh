04f0d46d7c2e7b71258e6a461e3ad6c297ac4f36
Stephan Boyer <stephan@stephanboyer.com>
2020-11-07 23:42:44 -0800

    Add a simple loading animation

--- frontend/src/loading_page/loading_page.tsx
+++ frontend/src/loading_page/loading_page.tsx
@@ -1,14 +1,51 @@
 import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+
+  to {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+`;
+
+const orbitRadius = 64;
+const orbitBorderWidth = 4;
+const planetRadius = 12;
+const planetOffset =
+  (orbitRadius * Math.SQRT2 - orbitRadius) / Math.SQRT2 -
+  planetRadius -
+  orbitBorderWidth / 2;
 
 const LoadingPageContainer = styled.div`
-  width: 480px;
-  margin: 64px auto;
-  color: #333333;
+  width: ${orbitRadius * 2}px;
+  height: ${orbitRadius * 2}px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  animation: ${rotate} 5s linear infinite;
+  border: ${orbitBorderWidth}px solid #eeeeee;
+  border-radius: 50%;
+  overflow: visible;
+
+  &::after {
+    display: block;
+    width: ${planetRadius * 2}px;
+    height: ${planetRadius * 2}px;
+    position: relative;
+    left: ${planetOffset}px;
+    top: ${planetOffset}px;
+    background-color: #cccccc;
+    border: 4px solid #ffffff;
+    border-radius: 50%;
+    content: '';
+  }
 `;
 
 const LoadingPage: FunctionComponent<{}> = () => {
-  return <LoadingPageContainer>Loading...</LoadingPageContainer>;
+  return <LoadingPageContainer />;
 };
 
 export default LoadingPage;
