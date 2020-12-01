ea5de693ddbd2d08996b36dc52f5906f5fe523db
Stephan Boyer <stephan@stephanboyer.com>
2020-12-01 03:49:13 -0800

    Update the Webpack configs

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/main.tsx',
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['.js', '.ts', '.tsx'],
     symlinks: false,
   },
   module: {
